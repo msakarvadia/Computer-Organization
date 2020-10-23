07d916e87b0cfe087cc70d6125144b9ca7985809
msakarvadia <mansi.sakarvadia@gmail.com>
2020-10-22 22:55:04 -0400

    finished lab 5

--- Lab 5/ex3.asm
+++ Lab 5/ex3.asm
@@ -6,8 +6,7 @@
   newline:	.ascii "\n"
   .align 4 # This ensures that the space character is in the lowest order byte of its word
   spaceChar:    .asciiz " "
-  
-  
+
 
 
 .text 0x3000
@@ -41,13 +40,68 @@ main:
 ##################################################################################
 ##   Insert code here for performing normalization of data                      ##
 ##################################################################################
-   
-   
+  	addi 	$t2, $0, 0
+  	la      $t1, array
+  	addi 	$t5, $0, 20	#min
+  	addi 	$t6, $0, 40	#max
+  	sub	$s1, $s1, $s0	#s1 has the newMax-newMin
+  	
+  	
+ loop:
+ 	#t4 must contain the value we want
+	addi 	$t4, $0, 0 	#we need $t4 to contain the value we want to replace the element with
+	lw	$t7, ($t1) 	#value of I
+	sub	$t8, $t6, $t5	#t8 has the max-min
+	div	$t8, $s1, $t8 	#t8 is now (newMax-newMin)/(max-min)
+	sub	$t4, $t7, $t5	#t4 contains I-min
+	mul	$t9, $t4, $t8	#t9 is now (I - Min)((newMax-newMin)/(Max - Min))
+	add	$t4, $t9, $s0	#t4 contains full calculated value
+	sw 	$t4, ($t1)
+	addi	$t1, $t1, 4	#This is how we jump to next address of array
+  	addi    $t2, $t2, 1
+  	slti	$t3, $t2, 16
+  	bne	$0, $t3, loop
+  	
 ##################################################################################
 ##   Insert code here for printing the normalized data. 			##
 ##   Separate each number with a space character.                               ##
 ##   You can adapt your ex2 printing for this part.				##
 ##################################################################################
+	addi, $t8, $0, 4 #number of charecters pre row
+	li      $v0, 0
+   	la      $t1, array
+rows:
+    addi $t7, $0, 0 #number of charecter we have printed in the row
+    col:
+    bge     $t0, 16, exit
+
+    # load word from addrs and goes to the next addrs
+    lw      $t2, 0($t1)
+    addi    $t1, $t1, 4
+
+    # syscall to print value
+    li      $v0, 1      
+    move    $a0, $t2
+    syscall
+    # syscall number for printing character (space)
+    li      $a0, 32
+    li      $v0, 11  
+    syscall
+
+    addi $t7, $t7, 1   #incremented number of charecters printed on a row
+
+    #increment counter
+    addi    $t0, $t0, 1
+    bne $t8, $t7, col  #if we have printed 8 char in a row, go to next row
+    j print_new_line   #print new line between rows
+    
+    
+print_new_line:
+	addi $v0, $0, 11
+	lb $a0, newline
+	syscall
+	j rows
+
 
 # Exit from the program
 exit:
